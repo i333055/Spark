c4faabace4a62b20e1e62c1e3993a298abf6e653
i333055 <lulu.ran@sap.com>
2021-08-31 18:32:26 +0800

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -2,6 +2,7 @@ module.exports = {
   "$schema": "https://docs.renovatebot.com/renovate-schema.json",
   "extends": ["local>i333055/renovate-presets:base(actiontest)"],
   "dryRun": false,
+  "renovateFork": true,
   "platform": "github",
   "repositories": ["i333055/Spark"],
   "includePaths": [
