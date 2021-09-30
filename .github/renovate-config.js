792f650b4103e753c5dc4a9e48ef454f6b3c8941
i333055 <lulu.ran@sap.com>
2021-09-30 11:24:25 +0800

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "$schema": "https://docs.renovatebot.com/renovate-schema.json",
-  "extends": ["local>i333055/renovate-presets:base(actiontest)"],
+  "extends": ["local>i333055/renovate-presets:base(maintanence)"],
   "onboarding": false,
   "requireConfig": false,
   "dryRun": false,
