f5bbde92c8df3dff16191c8f5d4221656d243a96
i333055 <lulu.ran@sap.com>
2022-06-23 18:08:02 +0800

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,6 +1,28 @@
 module.exports = {
-  "$schema": "https://docs.renovatebot.com/renovate-schema.json",
-  "extends": ["local>i333055/renovate-presets:base(maintanence)"],
+   "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+   "extends": [
+     "config:base",
+     ":preserveSemverRanges",
+     ":disableRateLimiting",
+     ":disableVulnerabilityAlerts",
+     ":rebaseStalePrs",
+     ":dependencyDashboard",
+     ":disableDevDependencies",
+     ":combinePatchMinorReleases",
+     "github>whitesource/merge-confidence:beta"
+   ],
+   "enabledManagers": ["nvm", "maven"],
+   "maven": {
+    "fileMatch": [
+      "\\.pom\\.xml$",
+      "(^|/)pom\\.xml$",
+      "(^|/)external-dependencies\\.xml$"
+    ]
+  },
+  "dependencyDashboardTitle": "SBG Dependency Dashboard",
+  "dependencyDashboardHeader": "This issue contains a list of Renovate updates and their statuses for sbg.",
+  "branchPrefix": "renovate-sbg/",
+  "labels": ["renovate", "sbg", "Holiday"],
   "onboarding": false,
   "requireConfig": false,
   "dryRun": false,
